d304931f9c7853322f09ba594fb2d82e322e9e21
ilahyani <lahyani799@gmail.com>
2023-09-13 08:35:26 +0100

    Prisma setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
-  controllers: [],
-  providers: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, PrismaModule],
 })
 export class AppModule {}
