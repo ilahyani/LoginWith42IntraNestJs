23e3f8c391a3109c7a3708599ace4a25d2094ba4
ilahyani <lahyani799@gmail.com>
2023-09-30 14:20:44 +0000

    Update Header

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col	items-center justify-center	 my-10">
-        <h1 className="text-5xl pb-10 "> 🥐 طخونسينضونص 🥐</h1>
+        <h1 className="text-5xl pb-10 "> HOME PAGE </h1>
         <Link href={"/auth/login"}>
           <button className="block bg-zinc-200 px-6 py-3 rounded-lg">
             {"LET'S GO 🔥"}
